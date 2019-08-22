c1a1589e6cb0ca6faeb84d84f842ca805f89ed08
Janill Lema <jl4817@columbia.edu>
2019-08-22 18:06:02 -0400

    fixed landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {Layout,Header,Navigation,Content,Drawer} from 'react-mdl';
+import Main from './components/main';
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="demo-big-content">
+        <Layout>
+            <Header title="Title" scroll>
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main/>
+            </Content>
+        </Layout>
     </div>
   );
 }
---
+++ src/components/about_me.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'React';
+
+class About extends Component{
+	render(){
+		return{
+			<div><h1> About Page </h1></div>
+		}
+	}
+}
+
+export default About;
